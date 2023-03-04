08b9a4ec0d064dc0595dc8b069e7f1a69f592270
Pauline Barnades <pauline.barnades@stenaline.com>
2023-03-03 21:54:06 -0700

    first commit- changing UI

--- packages/next-app/pages/components/Hero.jsx
+++ packages/next-app/pages/components/Hero.jsx
@@ -1,61 +1,90 @@
 import React from "react";
-import { 
+import {
   Heading,
-  Text, 
+  Text,
   Flex,
   Box,
   Button,
   Stack,
   Img,
-  useMediaQuery
+  useMediaQuery,
 } from "@chakra-ui/react";
 
+function redirectToNewURL() {
+  window.location.href = "https://http://localhost:3000/new-url";
+}
+
 function Hero() {
-  const [isLargerThanLG] = useMediaQuery('(min-width: 62em)');
+  const [isLargerThanLG] = useMediaQuery("(min-width: 62em)");
   return (
     <div>
       <Flex
         alignItems="center"
         w="full"
-        px={isLargerThanLG ? '19' : '8'}
+        px={isLargerThanLG ? "19" : "8"}
         py="10"
         minHeight="20vh"
-        flexDirection={isLargerThanLG ? 'row' : 'column'}>
-        <Box mr={isLargerThanLG ? '6' : '0'} w={isLargerThanLG ? '60%' : 'full'}>
+        flexDirection={isLargerThanLG ? "row" : "column"}
+      >
+        <Box
+          mr={isLargerThanLG ? "6" : "0"}
+          w={isLargerThanLG ? "60%" : "full"}
+        >
           {/* TITLE */}
           <Flex justifyContent={"left"} alignItems={"center"}>
             <Heading
-              className={"text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"}
-              fontSize={["4rem", "3.8rem", "3.5rem", "3.3rem", "3rem"]}>
-              Hola Mundo
+              className={
+                "text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+              }
+              fontSize={["4rem", "3.8rem", "3.5rem", "3.3rem", "3rem"]}
+            >
+              OrgaDao
             </Heading>
           </Flex>
           {/* DESCRIPTION */}
-          <Text mb="5" fontSize={isLargerThanLG ? 'lg' : 'base'} opacity={0.7}>
-            Una aplicación para enviar y recibir saludos!
+          <Text mb="5" fontSize={isLargerThanLG ? "lg" : "base"} opacity={0.7}>
+            Where DAOs and contributors can meet blablal We need to find a good
+            explanations here !! so judge understands straight away what problem
+            we solve
           </Text>
-          <Stack direction='row' spacing={4}>
-          {/* GREET THE WORLD */}
+          <Stack direction="row" spacing={4}>
+            {/* GREET THE WORLD */}
             <Button
               w="200px"
               colorScheme="blue"
               variant="solid"
               h="50px"
-              size={isLargerThanLG ? 'lg' : 'md'}
-              mb={isLargerThanLG ? '0' : '10'}
+              size={isLargerThanLG ? "lg" : "md"}
+              mb={isLargerThanLG ? "0" : "10"}
               onClick={(e) => {
                 e.preventDefault();
-                window.location.href='/create-greeting';
-                }}>
-              Saluda al Mundo 👋
+                window.location.href = "/create-greeting";
+              }}
+            >
+              We are a DAO
+            </Button>
+            <Button
+              w="200px"
+              colorScheme="blue"
+              variant="solid"
+              h="50px"
+              size={isLargerThanLG ? "lg" : "md"}
+              mb={isLargerThanLG ? "0" : "10"}
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href = "/create-contributorProfile";
+              }}
+            >
+              I am a contributor
             </Button>
           </Stack>
         </Box>
         {/* IMAGE */}
         <Flex
-          w={isLargerThanLG ? '40%' : '60%'}
+          w={isLargerThanLG ? "40%" : "60%"}
           alignItems="left"
-          justifyContent="left">
+          justifyContent="left"
+        >
           <Img src="/HolaMundo.png" alt="Hola Mundo Image" />
         </Flex>
       </Flex>
--- packages/next-app/pages/create-greeting.js
+++ packages/next-app/pages/create-greeting.js
@@ -1,78 +1,79 @@
 import React, { useState, useEffect } from "react";
 import {
-    Box,
-    Button,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    Select,
-    NumberInput,
-    NumberInputStepper,
-    NumberIncrementStepper,
-    NumberDecrementStepper,
-    NumberInputField,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    Stack,
-    Textarea,
-    VStack,
-    useColorModeValue,
-    useToast
-} from '@chakra-ui/react';
-  import Navbar from "./components/Navbar";
-  import Header from "./Header";
-  // Icon
-  import { BsPerson } from 'react-icons/bs';
-  // Data
-  import countriesJSON from '../data/countries.json';
-  import cryptocurrenciesJSON from '../data/cryptocurrencies.json';
-  // Helper
-  import getRandomImage from "../helpers/getRandomImage";
-  // Wagmi 
-  import { useContract, useSigner } from 'wagmi';
-  // Address + ABI 
-  import { contractAddress } from '../utils/contractAddress.js';
-  import contractABI from '../contracts/ABI/HelloWorld.json';
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Select,
+  NumberInput,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  NumberInputField,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+  Textarea,
+  VStack,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import Header from "./Header";
+// Icon
+import { BsPerson } from "react-icons/bs";
+// Data
+import countriesJSON from "../data/countries.json";
+import cryptocurrenciesJSON from "../data/cryptocurrencies.json";
+// Helper
+import getRandomImage from "../helpers/getRandomImage";
+// Wagmi
+import { useContract, useSigner } from "wagmi";
+// Address + ABI
+import { contractAddress } from "../utils/contractAddress.js";
+import contractABI from "../contracts/ABI/HelloWorld.json";
 
-  export default function Form() {
-    // Chakura-UI Toast Messages
-    const toast = useToast();
+export default function Form() {
+  // Chakura-UI Toast Messages
+  const toast = useToast();
 
-    // Toast For Every Page Render
-    useEffect(() => {
-        toast({
-          title: "Connect Wallet",
-          description: "Connect to Polygon Mumbai",
-          status: "info",
-          duration: 4000,
-          isClosable: false,
-          position: "bottom-right",
-        });
-      }, []);
-
-    // Transaction States
-    const [success, setSuccess] = useState(null);
-    const [loading, setLoading] = useState(null);
-    // Form States
-    const [personName, setName] = useState("");
-    const [personAge, setAge] = useState("");
-    const [personCountry, setCountry] = useState("");
-    const [faveCrypto, setCrypto] = useState("");
-    const [message, setMessage] = useState("");
-
-    // Connect To Contract
-    const signer = useSigner();
-    const contractOnMumbai = useContract({
-      addressOrName: contractAddress,
-      contractInterface: contractABI,
-      signerOrProvider: signer.data,
+  // Toast For Every Page Render
+  useEffect(() => {
+    toast({
+      title: "Connect Wallet",
+      description: "Connect to Polygon Mumbai",
+      status: "info",
+      duration: 4000,
+      isClosable: false,
+      position: "bottom-right",
     });
+  }, []);
+
+  // Transaction States
+  const [success, setSuccess] = useState(null);
+  const [loading, setLoading] = useState(null);
+  // Form States
+  const [personName, setName] = useState("");
+  const [personSkill, setPersonSkill] = useState("");
+  const [personAge, setAge] = useState("");
+  const [personCountry, setCountry] = useState("");
+  const [faveCrypto, setCrypto] = useState("");
+  const [message, setMessage] = useState("");
+
+  // Connect To Contract
+  const signer = useSigner();
+  const contractOnMumbai = useContract({
+    addressOrName: contractAddress,
+    contractInterface: contractABI,
+    signerOrProvider: signer.data,
+  });
 
   // Toasts for Transaction States
   useEffect(() => {
-    if(success) {
+    if (success) {
       toast({
         title: "Success!",
         status: "success",
@@ -81,7 +82,7 @@ import {
         position: "bottom-right",
       });
     }
-    if(loading) {
+    if (loading) {
       toast({
         title: "Waiting...",
         status: "loading",
@@ -92,160 +93,192 @@ import {
     }
   }, [success, loading]);
 
-    // Handle Submit
-    async function handleSubmit(e) {
-      e.preventDefault();
+  // Handle Submit
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-      const body = {
-        name: personName,
-        age: personAge,
-        country: personCountry,
-        crypto: faveCrypto,
-        formMessage: message,
-        image: getRandomImage()
-      };
-
-      console.log("BODY: ", body);
-
-      try {
-        // Save Form Details In IPFS
-        const response = await fetch("/api/StoreData", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
-        });
-
-        if (response.status !== 200) {
-          alert("Oops! Something went wrong. Please refresh & try again.");
-        } else {
-          let responseJSON = await response.json();
-          await createGreeting(responseJSON.cid);
-          console.log("Saved in IPFS: ", responseJSON.cid);
-        }
-      } catch (error) {
-        alert("Oops! Something went wrong. Please refresh & try again.", error);
-      }
-    }
-
-    // Create Greeting
-    const createGreeting = async (cid) => {
-      try {
-        // Reset
-        setSuccess(false)
-        setLoading(false)
-        if (contractOnMumbai) {
-          // Calling smart contract function: createNewGreeting
-          const txn = await contractOnMumbai.createNewGreeting(cid,{ gasLimit: 900000 });
-          setLoading(true);
-          await txn.wait();
-          setLoading(false);
-          setSuccess(true);
-        } else {
-          setSuccess(false)
-          setLoading(false)
-          alert("Oops! Something went wrong. Please refresh & try again.");
-        }
-      } catch (error) {
-        setSuccess(false)
-        setLoading(false)
-        alert("Oops! Something went wrong. Please refresh & try again.");
-      }
+    const body = {
+      name: personName,
+      age: personAge,
+      country: personCountry,
+      crypto: faveCrypto,
+      formMessage: message,
+      image: getRandomImage(),
     };
 
-    // UI
-    return (
+    console.log("BODY: ", body);
+
+    try {
+      // Save Form Details In IPFS
+      const response = await fetch("/api/StoreData", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      if (response.status !== 200) {
+        alert("Oops! Something went wrong. Please refresh & try again.");
+      } else {
+        let responseJSON = await response.json();
+        await createGreeting(responseJSON.cid);
+        console.log("Saved in IPFS: ", responseJSON.cid);
+      }
+    } catch (error) {
+      alert("Oops! Something went wrong. Please refresh & try again.", error);
+    }
+  }
+
+  // Create Greeting
+  const createGreeting = async (cid) => {
+    try {
+      // Reset
+      setSuccess(false);
+      setLoading(false);
+      if (contractOnMumbai) {
+        // Calling smart contract function: createNewGreeting
+        const txn = await contractOnMumbai.createNewGreeting(cid, {
+          gasLimit: 900000,
+        });
+        setLoading(true);
+        await txn.wait();
+        setLoading(false);
+        setSuccess(true);
+      } else {
+        setSuccess(false);
+        setLoading(false);
+        alert("Oops! Something went wrong. Please refresh & try again.");
+      }
+    } catch (error) {
+      setSuccess(false);
+      setLoading(false);
+      alert("Oops! Something went wrong. Please refresh & try again.");
+    }
+  };
+
+  // UI
+  return (
     <div>
-    <Header/>
-    <Navbar/>
-      <Flex
-        align="center"
-        justify="center"
-        id="intro">
+      <Header />
+      <Navbar />
+      <Flex align="center" justify="center" id="intro">
         <Box
           borderRadius="lg"
           m={{ base: 5, md: 16, lg: 10 }}
-          p={{ base: 5, lg: 16 }}>
+          p={{ base: 5, lg: 16 }}
+        >
           <Box>
             <VStack spacing={{ base: 2, md: 4, lg: 8 }}>
               {/* TITLE */}
               <Heading
-                className={"text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"}
+                className={
+                  "text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+                }
                 fontSize={{
-                  base: '3xl',
-                  md: '5xl',
-                }}>
-                Saluda al Mundo 👋
+                  base: "3xl",
+                  md: "5xl",
+                }}
+              >
+                Contributor profile
               </Heading>
               {/* FORM */}
               <Stack
                 spacing={{ base: 4, md: 7, lg: 10 }}
-                direction={{ base: 'column', md: 'row' }}>
+                direction={{ base: "column", md: "row" }}
+              >
                 <Box
-                  borderWidth='1px'
-                  bg={useColorModeValue('white', 'gray.700')}
+                  borderWidth="1px"
+                  bg={useColorModeValue("white", "gray.700")}
                   borderRadius="lg"
                   p={8}
-                  color={useColorModeValue('gray.700', 'whiteAlpha.900')}
-                  shadow="base">
-                  {/* FIELD: NOMBRE */}
-                  <VStack spacing={5}>][]
+                  color={useColorModeValue("gray.700", "whiteAlpha.900")}
+                  shadow="base"
+                >
+                  {/* FIELD: NAme */}
+                  <VStack spacing={6}>
+                    ][]
                     <FormControl isRequired>
-                      <FormLabel>Nombre</FormLabel>
+                      <FormLabel>Name</FormLabel>
                       <InputGroup>
                         <InputLeftElement>
-                            <BsPerson />
+                          <BsPerson />
                         </InputLeftElement>
-                        <Input 
-                          type="text" 
-                          name="name" 
-                          placeholder="Su Nombre"
+                        <Input
+                          type="text"
+                          name="name"
+                          placeholder="Please enter your name"
                           onChange={(e) => setName(e.target.value)}
+                        />
+                      </InputGroup>
+                    </FormControl>
+                    <FormControl isRequired>
+                      <FormLabel>Skills</FormLabel>
+                      <InputGroup>
+                        <InputLeftElement>
+                          <BsPerson />
+                        </InputLeftElement>
+                        <Input
+                          type="text"
+                          name="name"
+                          placeholder="Category you wish to contribute to"
+                          onChange={(e) => setPersonSkill(e.target.value)}
                         />
                       </InputGroup>
                     </FormControl>
                     {/* FIELD: EDAD / AGE */}
                     <FormControl isRequired>
-                      <FormLabel>Edad</FormLabel>
-                      <NumberInput 
-                        allowMouseWheel="true" 
-                        max={110} min={1} 
-                        defaultValue="1">
-                        <NumberInputField onChange={(e) => setAge(e.target.value)}/>
+                      <FormLabel>Category</FormLabel>
+                      <NumberInput
+                        allowMouseWheel="true"
+                        max={110}
+                        min={1}
+                        defaultValue="1"
+                      >
+                        <NumberInputField
+                          onChange={(e) => setAge(e.target.value)}
+                        />
                         <NumberInputStepper>
-                        <NumberIncrementStepper />
-                        <NumberDecrementStepper />
+                          <NumberIncrementStepper />
+                          <NumberDecrementStepper />
                         </NumberInputStepper>
-                    </NumberInput>
+                      </NumberInput>
                     </FormControl>
                     {/* FIELD: PAÍS / COUNTRY */}
                     <FormControl isRequired>
-                      <FormLabel>País</FormLabel>
-                        <Select
+                      <FormLabel>Another field ??</FormLabel>
+                      <Select
                         onChange={(e) => setCountry(e.target.value)}
-                        placeholder='Selecciona País'>
-                            {countriesJSON.map((country) => {
-                                return (<option key={country.code}>{country.name}</option>);
-                            })}
-                        </Select>
+                        placeholder="Selecciona País"
+                      >
+                        {countriesJSON.map((country) => {
+                          return (
+                            <option key={country.code}>{country.name}</option>
+                          );
+                        })}
+                      </Select>
                     </FormControl>
                     {/* FIELD: CRIPTO / CRYPTO */}
                     <FormControl isRequired>
-                      <FormLabel>¿Cuál Es Tu Criptomoneda Favorita?</FormLabel>
-                      <Select 
-                      onChange={(e) => setCrypto(e.target.value)}
-                      placeholder='Selecciona Criptomoneda'>
-                            {cryptocurrenciesJSON.map((crypto) => {
-                                return (<option key={crypto.code}>{crypto.name}</option>);
-                            })}
-                        </Select>
+                      <FormLabel>
+                        Which crypto you wish to be paid with
+                      </FormLabel>
+                      <Select
+                        onChange={(e) => setCrypto(e.target.value)}
+                        placeholder="Select the following"
+                      >
+                        {cryptocurrenciesJSON.map((crypto) => {
+                          return (
+                            <option key={crypto.code}>{crypto.name}</option>
+                          );
+                        })}
+                      </Select>
                     </FormControl>
                     {/* FIELD: MENSAJE / MESSAGE */}
                     <FormControl isRequired>
-                      <FormLabel>Mensaje</FormLabel>
+                      <FormLabel>Message</FormLabel>
                       <Textarea
                         onChange={(e) => setMessage(e.target.value)}
                         name="Message"
-                        placeholder="Tu Mensaje"
+                        placeholder="Add valuable info for the DAO"
                         rows={6}
                         resize="none"
                       />
@@ -256,10 +289,11 @@ import {
                       bg="blue.400"
                       color="white"
                       _hover={{
-                        bg: 'blue.500',
+                        bg: "blue.500",
                       }}
-                      onClick={(e)=> handleSubmit(e)}>
-                      Crear Saludo
+                      onClick={(e) => handleSubmit(e)}
+                    >
+                      Create profile
                     </Button>
                   </VStack>
                 </Box>
@@ -269,5 +303,5 @@ import {
         </Box>
       </Flex>
     </div>
-    );
-  }
+  );
+}
